2be4b432b3126d3f2f010bdff8724f93e8373cdf
Vladlena Panchenko <57011546+lazy-ocean@users.noreply.github.com>
2022-05-27 21:19:27 +0200

    feat: add main copy

--- src/components/Main/Main.styled.ts
+++ src/components/Main/Main.styled.ts
@@ -1,12 +1,17 @@
 import styled from "styled-components";
 
-export const Header = styled.h4`
-  font-size: 2rem;
-  font-weight: 200;
+export const Container = styled.main`
+  padding: ${({ theme }) => `${theme.spacings.s} ${theme.spacings.m}`};
+  display: flex;
+  gap: ${({ theme }) => theme.spacings.m};
 `;
 
-export const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
+export const Copy = styled.div`
+  font-size: 1.2rem;
+  line-height: 2.2rem;
+  max-width: 40%;
+`;
+
+export const WTTLink = styled.a`
+  border-bottom: 2px dashed #bc98cb;
 `;
--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,13 +1,28 @@
 import React from "react";
-import { Header, Container } from "./Main.styled";
+import { Container, Copy, WTTLink } from "./Main.styled";
 
 const Main = () => {
   return (
-    <main>
-      <Container>
-        <Header>start here</Header>
-      </Container>
-    </main>
+    <Container>
+      <Copy>
+        <h1>
+          Hello! 👋
+          <br />I am Vladlena Panchenko
+        </h1>
+        I&apos;m a Frontend Engineer at{" "}
+        <WTTLink
+          href="https://www.wundermanthompson.com/expertise/technology"
+          target="_blank"
+          rel="noopener nofollow noreferrer"
+        >
+          Wunderman Thompson Technology
+        </WTTLink>
+        .<br />I love developing complex and accessible web-interfaces and
+        always eager to try something new.
+        <br />
+        Siberian-born living in Gdansk, Poland.
+      </Copy>
+    </Container>
   );
 };
 
