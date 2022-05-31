12cf2be214eae3d9fe3ebf26cb3f1ea8934c2056
Vladlena Panchenko <57011546+lazy-ocean@users.noreply.github.com>
2022-05-31 15:39:09 +0200

    fix: met og fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,38 +6,35 @@ import { ThemeProvider } from "styled-components";
 import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer";
 
-const MyApp = () => {
-  /* let testLintIsWorking = ""; */
-  return (
-    <>
-      <Head>
-        <title>Vladlena | Frontend Engineer</title>
-        <meta
-          name="description"
-          content="Hello! 👋
+const MyApp = () => (
+  <>
+    <Head>
+      <title>Vladlena | Frontend Engineer</title>
+      <meta
+        name="description"
+        content="Hello! 👋
           I am Vladlena Panchenko and I am a frontent engineer"
-        />
-        <meta
-          property="og:description"
-          content="Hello! 👋
+      />
+      <meta
+        property="og:description"
+        content="Hello! 👋
         I am Vladlena Panchenko and I am a frontent engineer"
-        />
-        <meta property="og:url" content="https://lazy-ocean.github.io/" />
-        <meta property="og:title" content="Vladlena | Frontend engineer" />
-        <meta property="og:type" content="website" />
-        <meta property="og:image" content="/Meta-photo.png" />
-        <link rel="icon" href="/favicon.ico" sizes="any" />
-        <link rel="icon" href="/icon.png" type="image/png" />
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-      </Head>
-      <ThemeProvider theme={theme}>
-        <GlobalStyles />
-        <Header />
-        <Main />
-        <Footer />
-      </ThemeProvider>
-    </>
-  );
-};
+      />
+      <meta property="og:url" content="https://lazy-ocean.vercel.app/" />
+      <meta property="og:title" content="Vladlena | Frontend engineer" />
+      <meta property="og:type" content="website" />
+      <meta property="og:image" content="/meta-img.png" />
+      <link rel="icon" href="/favicon.ico" sizes="any" />
+      <link rel="icon" href="/icon.png" type="image/png" />
+      <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+    </Head>
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <Header />
+      <Main />
+      <Footer />
+    </ThemeProvider>
+  </>
+);
 
 export default MyApp;
