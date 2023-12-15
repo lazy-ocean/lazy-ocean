fc020b0e271676593560d753163a858dc8fe578d
Vladlena Panchenko <57011546+lazy-ocean@users.noreply.github.com>
2023-12-15 21:06:25 +0100

    feat: copy email functionality

---
+++ src/components/2023/Footer/Email.tsx
@@ -0,0 +1,23 @@
+"use client";
+import React, { useState } from "react";
+import styles from "./Footer.module.css";
+
+const Email = () => {
+  const [copied, setCopied] = useState(false);
+
+  const handleClick = () => {
+    setCopied(true);
+    if (typeof window !== "undefined") {
+      window.navigator.clipboard.writeText("panchenko.vladlena@gmail.com");
+    }
+    setTimeout(() => setCopied(false), 2000);
+  };
+
+  return (
+    <button onClick={handleClick} className={styles.email}>
+      {copied ? "Copied to clipboard" : "panchenko.vladlena@gmail.com"}
+    </button>
+  );
+};
+
+export default Email;
--- src/components/2023/Footer/Footer.tsx
+++ src/components/2023/Footer/Footer.tsx
@@ -2,13 +2,12 @@ import React from "react";
 import { AiFillHeart } from "react-icons/ai";
 import SocialLinks from "../SocialLinks/SocialLinks";
 import styles from "./Footer.module.css";
+import Email from "./Email";
 
 const Footer = ({ cv }: { cv: string }) => {
   return (
     <footer className={styles.container}>
-      <a href="mailto:panchenko.vladlena@gmail.com" className={styles.email}>
-        panchenko.vladlena@gmail.com
-      </a>
+      <Email />
       <div className={styles.copyright}>
         <p>Vladlena Panchenko</p>
         <AiFillHeart />
