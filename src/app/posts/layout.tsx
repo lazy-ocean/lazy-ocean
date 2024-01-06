db93abd24fd4c838146ab482a5584342bd20d43d
Vladlena Panchenko <57011546+lazy-ocean@users.noreply.github.com>
2024-01-06 16:25:31 +0100

    feat: add footer to blog posts / posts page

--- src/app/posts/layout.tsx
+++ src/app/posts/layout.tsx
@@ -1,12 +1,12 @@
-import { Header } from "../../components/2023/Header/Header";
+import Footer from "../../components/2023/Footer/Footer";
 import styles from "./posts.module.css";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={styles.container}>
-        <Header />
-        {children}
+      <body>
+        <div className={styles.container}>{children}</div>
+        <Footer cv="/Vladlena_Panchenko_CV.pdf" />
       </body>
     </html>
   );
