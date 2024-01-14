0e643da49a501f57001ebddb0f4d71f10c256273
Vladlena Panchenko <57011546+lazy-ocean@users.noreply.github.com>
2024-01-14 14:26:50 +0100

    fix: better markdown parsing

--- src/utils/markdownToHtml.ts
+++ src/utils/markdownToHtml.ts
@@ -1,9 +1,18 @@
 import { remark } from "remark";
-import html from "remark-html";
+import remarkRehype from "remark-rehype";
+import remarkParse from "remark-parse";
+import rehypeStringify from "rehype-stringify";
+import rehypeRaw from "rehype-raw";
 
 export default async function markdownToHtml(
   markdown: string
 ): Promise<string> {
-  const result = await remark().use(html).process(markdown);
+  const result = await remark()
+    .use(remarkParse)
+    //@ts-ignore
+    .use(remarkRehype, { allowDangerousHtml: true })
+    .use(rehypeRaw)
+    .use(rehypeStringify)
+    .process(markdown);
   return result.toString();
 }
